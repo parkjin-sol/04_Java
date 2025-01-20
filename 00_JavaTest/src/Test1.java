b78397827f3ece0cb9b671fa84ad09d1c5dbb45f
parkjinsol <soljinpark1223@gmail.com>
2025-01-20 11:25:44 +0900

    java workspace 등록

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,29 @@
+// JS와 주석 동일
+
+/*Run 버튼 클릭 시 (단축키 ctrl + f11)
+ * 1) 자동으로 컴파일 수행(javac ooo.java)
+ * 	-> .class 파일(byte code) 생성
+ * 
+ * 2) 컴파일 완료 후 .class 파일 실행 (java class 파일명)
+ * */
+
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("이클립스 실행 중");
+		System.out.println("Hello");
+
+	}
+
+}
+/* Java API DOC
+ * Java에서 제공해주는 코드의 사용법이 설명 되어있는 문서
+ * 
+ * DOC 확인 방법
+ * 1) google -> java 21 api 검색
+ * 
+ * 2) 궁금한 코드 클릭 ->  shift + f2
+ */
---
+++ JavaTest.java
@@ -0,0 +1,15 @@
+public class JavaTest{
+
+	public static void main(String[] args){
+		
+		System.out.println("Hello World!!!"); 
+		System.out.println("월요병 지대로"); 
+		System.out.println("아,, 힘듭니다"); 
+
+		for(int i=1 ; i<=5 ; i++) {
+			System.out.println( i + "번째 줄 출력");
+		}
+
+	}
+
+}
